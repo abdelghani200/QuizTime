d1747aaff77ef374ff7dcdf14866e7c4af8b3750
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-10-27 22:43:12 +0100

    setup project  & readme file

---
+++ src/test/java/com/QuizTime/QuizTime/QuizTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuizTime.QuizTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
